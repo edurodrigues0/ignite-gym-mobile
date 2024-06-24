c4042d4e2e96ae1e3620eb8870ad455984e5c8c6
edurodrigues0 <edusacramento9@gmail.com>
2024-06-24 19:40:59 -0300

    Finzalindo screen de Perfil

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,13 @@
 import { TextInput, TextInputProps } from 'react-native'
 
-type Props = TextInputProps & {}
+type Props = TextInputProps & {
+  bg?: string;
+}
 
-export function Input({...rest}: Props) {
+export function Input({bg = "bg-gray-700", ...rest}: Props) {
   return (
-    <TextInput 
-      className='bg-gray-700 w-full h-14 px-4 rounded text-md text-white font-body mb-4 placeholder:text-gray-300 focus:bg-gray-700 focus:border focus:border-green-500'
+    <TextInput
+      className={`w-full h-14 px-4 rounded text-md text-white font-body mb-4 placeholder:text-gray-300 focus:bg-gray-700 focus:border focus:border-green-500 ${bg}`}
       {...rest}
     />
   )
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,88 @@
-import { View, Text } from "react-native"
+import { ScreenHeader } from "@components/ScreenHeader"
+import { UserPhoto } from "@components/UserPhoto"
+import { useState } from "react"
+import { View, ScrollView, TouchableOpacity, Text } from "react-native"
+import { Input } from "@components/Input"
+import { Button } from "@components/Button"
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-700">
-      <Text className="text-white">Profile</Text>
+    <View className="flex-1 bg-gray-700">
+      <ScreenHeader title="Profile" />
+
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 156
+        }}
+      >
+        <View className="items-center mt-6 px-10">
+          {
+            photoIsLoading ? (
+              <View
+                className="bg-slate-700 h-36 w-36 rounded-full animate-bounce"
+              />
+            ) : (
+              <UserPhoto
+                source={{
+                  uri: "https://github.com/edurodrigues0.png"
+                }}
+                alt="Foto do usuário"
+                size={144}
+                className="h-36 w-36"
+              />
+            )
+          }
+
+          <TouchableOpacity className="mt-1 mb-7">
+            <Text className="text-green-500 text-md font-bold p-1">
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input
+            bg="bg-gray-600"
+            placeholder="Nome"
+          />
+
+          <Input
+            bg="bg-gray-600/40"
+            placeholder="edurodriguesdev1@gmail.com"
+            editable={false}
+          />
+        </View>
+
+        <View className="px-10 mt-12 mb-9">
+          <Text className="text-gray-200 text-md font-heading mb-2">
+            Alterar senha
+          </Text>
+
+          <Input
+            bg="bg-gray-600"
+            placeholder="Senha antiga"
+            secureTextEntry
+          />
+
+          <Input
+            bg="bg-gray-600"
+            placeholder="Nova senha"
+            secureTextEntry
+          />
+
+          <Input
+            bg="bg-gray-600"
+            placeholder="Confirme nova senha"
+            secureTextEntry
+          />
+
+          <View className="mt-4">
+            <Button
+              title="Atualizar"
+            />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   )
 }
